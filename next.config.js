ff5a0d94e6766f7e8f2d91c37c07d401d3a16b18
Dmitriy Minaev <minaevdup@gmail.com>
2020-03-03 17:27:33 +0300

    Alias

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const path = require('path');
 const withOffline = require('next-offline');
 
-module.exports = withOffline();
+module.exports = {
+  webpack: config => {
+    config.resolve.alias['components'] = path.join(
+      __dirname,
+      'src',
+      'components'
+    );
+
+    return config;
+  },
+  withOffline
+};
