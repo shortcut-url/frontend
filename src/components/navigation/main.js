310016313b24799f7bb5f1d05d4ba5cb109c2e78
Dmitriy Minaev <minaevdup@gmail.com>
2020-03-08 21:42:00 +0300

    Incorrect semantic layout of navigation, button accent

--- src/components/navigation/main.js
+++ src/components/navigation/main.js
@@ -10,61 +10,47 @@ import buttonsStyles from 'components/button/index.module.css';
 export const MainNavigation = () => {
   let user = useStore($session);
 
-  function Profile() {
-    if (!user) return null;
+  let Profile = () => (
+    <div className={styles.profile}>
+      <img
+        src="/images/no-avatar.jpg"
+        alt="Your avatar"
+        className={styles['profile_avatar']}
+        width="100"
+        height="100"
+      />
+      <div className={styles['profile_username']} title="Your username">
+        {user.username}
+      </div>
 
-    return (
-      <li className={styles.profile}>
-        <img
-          src="/images/no-avatar.jpg"
-          alt="Your avatar"
-          className={styles['profile_avatar']}
-          width="100"
-          height="100"
-        />
-        <div className={styles['profile_username']}>{user.username}</div>
-        <Link href="/profile">
-          <a className={buttonsStyles['button_accent']}>Your profile</a>
-        </Link>
-      </li>
-    );
-  }
+      <Link href="/profile">
+        <a className={`${buttonsStyles['button_accent']} profile_button`}>
+          Your profile
+        </a>
+      </Link>
+    </div>
+  );
 
-  function AccountMenu() {
-    if (user) return null;
-
-    return (
-      <>
-        <ListHeading nameIcon="user">Account</ListHeading>
-        <li>
-          <MenuLink href="/join">Create an account</MenuLink>
-        </li>
-        <li>
-          <MenuLink href="/login">Login</MenuLink>
-        </li>
-      </>
-    );
-  }
+  let AuthNavigationMenu = () => (
+    <>
+      <ListHeading nameIcon="user">Account</ListHeading>
+      <ul className={styles.list}>
+        <ListLink href="/join">Create an account</ListLink>
+        <ListLink href="/login">Login</ListLink>
+      </ul>
+    </>
+  );
 
   return (
-    <nav className={`${styles.nav} flat`}>
-      <div className={styles['layout_menu']}>
-        <ul className={styles.menu}>
-          <li>
-            <Link href="/">
-              <a className={styles['menu_home']}>
-                <Icon name="globe" size="23" /> Shortcut-link.com
-              </a>
-            </Link>
-          </li>
-          <ListHeading nameIcon="box">Main</ListHeading>
-          <li>
-            <MenuLink href="/">Home</MenuLink>
-          </li>
-          <AccountMenu />
-          <Profile />
-        </ul>
-      </div>
+    <nav className={`${styles.menu} flat`}>
+      <Link href="/">
+        <a className={styles['menu_home']} title="Go to the home page.">
+          <Icon name="globe" size="23" /> Shortcut-link.com
+        </a>
+      </Link>
+
+      {user && <AuthNavigationMenu />}
+      {user && <Profile />}
     </nav>
   );
 };
@@ -78,15 +64,17 @@ function ListHeading({ nameIcon, children }) {
   );
 }
 
-function MenuLink({ href, children, ...props }) {
+function ListLink({ href, children, ...props }) {
   return (
-    <Link href={href}>
-      <a
-        className={`${styles['menu_link']} concave_hover pressed_active`}
-        {...props}
-      >
-        {children}
-      </a>
-    </Link>
+    <li>
+      <Link href={href}>
+        <a
+          className={`${styles['list_link']} concave_hover pressed_active`}
+          {...props}
+        >
+          {children}
+        </a>
+      </Link>
+    </li>
   );
 }
